87dbfb5d3d03a09a97cc0c306826f2ad831c0bcd
zhangzhongming <810307015@qq.com>
2020-11-16 12:18:28 +0800

    添加echarts组件

--- pages/weightPage/weightPage.js
+++ pages/weightPage/weightPage.js
@@ -1,4 +1,6 @@
 // pages/weightPage/weightPage.js
+const util = require('../../utils/util.js');
+const echarts = require('../../components/ec-canvas/echarts.js')
 Page({
 
   /**
@@ -6,23 +8,169 @@ Page({
    */
   data: {
     weightList: [], // 体重列表
+    showList: [], // 展示的体重列表
     showModal: false, // 是否显示modal
     formData: {}, // 表单数据
+    people: ['全部'], // 记录人列表
+    currentPerson: '全部', // 当前选择的记录人
+    showType: 0, // 0 列表展示， 1 echarts展示
+    ec: {}, // echarts图的配置
   },
 
-  showAdd: function() {
+  // 初始化echarts图的配置
+  initChart: function (canvas, width, height, dpr) {
+    const chart = echarts.init(canvas, null, {
+      width: width,
+      height: height,
+      devicePixelRatio: dpr // 像素
+    });
+    canvas.setChart(chart);
+    const list = this.data.weightList;
+    const people = this.data.people;
+    let xAxisData = [],
+        minY = 100,
+        maxY = 0;
+    const series = people.filter(item => item !== '全部').map(person => {
+      const personList = list.filter(item => item.name === person).sort((a, b) => (+new Date(a.time)) - (+new Date(b.time)));
+      const timeList = personList.map(item => util.formatTime(new Date(item.time)));
+      const valueList = personList.map(item => item.range);
+      const min = Math.min.apply(null, valueList);
+      const max = Math.max.apply(null, valueList);
+      minY = minY < min ? minY : min;
+      maxY = maxY > max ? maxY : max;
+      xAxisData.push(...timeList);
+      return {
+        type: 'line',
+        name: person,
+        data: personList
+      }
+    });
+    xAxisData = [ ...new Set(xAxisData) ];
+    series.forEach(item => {
+      const { data } = item;
+      const _data = [...data];
+      const newData = [];
+      xAxisData.forEach((time, index) => {
+        const fItem = _data.find(item => util.formatTime(new Date(item.time)) === time);
+        newData[index] = fItem ? fItem.range : null;
+      });
+      item.data = newData;
+    })
+    const option = {
+      color: ['#0694F3', '#5BE2D4', '#7FC73A'],
+      grid: {
+        top: 45,
+        left: 50,
+        right: 10,
+        bottom: 40
+      },
+      legend: {
+        show: true
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+          shadowStyle: {
+            color: 'rgba(0, 112, 251, 0.05)'
+          }
+        },
+        formatter: (params) => {
+          let time = '',
+              list = [];
+          params.forEach(param => {
+            time = param.axisValue;
+            list.push({
+              name: param.seriesName,
+              value: param.value || '暂无记录'
+            })
+          });
+          let valueStr = '';
+          list.forEach(item => {
+            valueStr += `${item.name}：${item.value}\n`
+          })
+
+          return `${time}\n${valueStr}`;
+        }
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: xAxisData,
+        axisLine: {
+          lineStyle: {
+            color: '#EFF1F4',
+            type: 'dashed'
+          }
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: '#EFF1F4',
+            type: 'solid'
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          color: '#B0B1B9'
+        }
+      },
+      yAxis: {
+        name: '',
+        type: 'value',
+        position: 'left',
+        min: minY,
+        max: maxY,
+        axisTick: {
+          show: false
+        },
+        axisLine: {
+          lineStyle: {
+            color: '#EFF1F4',
+            type: 'solid'
+          }
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: '#EFF1F4',
+            type: 'dashed'
+          }
+        },
+        axisLabel: {
+          color: '#B0B1B9'
+        },
+        nameTextStyle: {
+          color: '#B0B1B9'
+        }
+      },
+      series: series
+    };
+    chart.setOption(option);
+    return chart;
+  },
+
+  changeShowType: function () {
+    this.setData({
+      showType: this.data.showType === 0 ? 1 : 0
+    })
+  },
+
+  showAdd: function () {
     this.setData({
       showModal: true
     })
   },
 
-  toggleModal: function() {
+  toggleModal: function () {
     this.setData({
       showModal: !this.data.showModal
     })
   },
 
-  formInputChange: function(e) {
+  formInputChange: function (e) {
     const value = e.detail.value;
     const field = e.currentTarget.dataset.field;
     this.setData({
@@ -33,18 +181,18 @@ Page({
     });
   },
 
-  addRecord: function() {
-    const { name, range } = this.data.formData;
-    if(!name || !range) {
+  addRecord: function () {
+    const {
+      name,
+      range
+    } = this.data.formData;
+    if (!name || !range) {
       wx.showToast({
         title: '输入框不能为空',
       });
       return;
     }
     const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const day = now.getDate();
     wx.showLoading({
       title: '新增记录中...',
     })
@@ -56,7 +204,7 @@ Page({
         data: {
           name,
           range,
-          time: `${year}-${month}-${day}`
+          time: util.formatTime(now)
         }
       },
       success: () => {
@@ -74,7 +222,25 @@ Page({
     })
   },
 
+  // 切换显示的列表
+  changeShowList(e) {
+    const {
+      person
+    } = e.target.dataset;
+    let showList = this.data.weightList;
+    if (person !== '全部') {
+      showList = showList.filter(item => item.name === person);
+    }
+    this.setData({
+      showList,
+      currentPerson: person
+    })
+  },
+
   init() {
+    wx.showLoading({
+      title: '获取记录中...',
+    })
     wx.cloud.callFunction({
       name: 'tableOperate',
       data: {
@@ -82,9 +248,19 @@ Page({
         cName: 'weight'
       },
       success: (res) => {
+        wx.hideLoading()
         const list = res.result.data || [];
+        list.sort((a, b) => {
+          return (+new Date(b.time)) - (+new Date(a.time));
+        });
+        const people = ['全部', ...new Set(list.map(item => item.name))];
         this.setData({
-          weightList: list
+          weightList: list,
+          showList: list,
+          people,
+          ec: {
+            onInit: this.initChart
+          }
         })
       }
     })
--- utils/util.js
+++ utils/util.js
@@ -1,12 +1,15 @@
-const formatTime = date => {
+const formatTime = (date, isNeedTime = false) => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
   const hour = date.getHours()
   const minute = date.getMinutes()
   const second = date.getSeconds()
+  if(isNeedTime) {
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  }
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('/');
 }
 
 const formatNumber = n => {
